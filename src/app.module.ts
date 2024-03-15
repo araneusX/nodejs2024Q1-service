28f1d566042cfac350e2c6397df9cd2be14d206f
araneus-web <a.oleshkevish@kernelics.com>
2024-03-16 00:38:31 +0300

    feat: use db migrations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,13 @@
-import { Logger, Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { Module } from '@nestjs/common';
 import { UserModule } from './modules/user/user.module';
 import { ArtistModule } from './modules/artist/artist.module';
 import { TrackModule } from './modules/track/track.module';
 import { AlbumModule } from './modules/album/album.module';
 import { FavoritesModule } from './modules/favorites/favorites.module';
-import { AlbumEntity } from './modules/album';
-import { ArtistEntity } from './modules/artist';
-import { FavoritesEntity } from './modules/favorites';
-import { TrackEntity } from './modules/track';
-import { UserEntity } from './modules/user';
 import { ConfigModule } from '@nestjs/config';
+import { DbModule } from './modules/db/db.module';
 import { EnvService } from './utils';
+
 @Module({
   providers: [EnvService],
   imports: [
@@ -23,28 +19,7 @@ import { EnvService } from './utils';
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    TypeOrmModule.forRootAsync({
-      extraProviders: [EnvService],
-      inject: [EnvService],
-      useFactory(env: EnvService) {
-        return {
-          type: 'postgres',
-          host: env.DB_HOST,
-          port: env.DB_PORT,
-          username: env.DB_USERNAME,
-          password: env.DB_PASSWORD,
-          database: env.DB_NAME,
-          entities: [
-            AlbumEntity,
-            ArtistEntity,
-            FavoritesEntity,
-            TrackEntity,
-            UserEntity,
-          ],
-          synchronize: true,
-        };
-      },
-    }),
+    DbModule,
   ],
 })
 export class AppModule {}
